7e3a96754bdead57df3bb6777ca092ed8a65ad57
AmitBokoDev <amitbokodev@gmail.com>
2023-01-10 13:11:52 +0200

    created basic client side game from open source

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import board from './components/board';
-import button from './components/button';
+
+import { Board } from './components/board'
+
+import './styles/board.css'
+import './styles/box.css'
+import './styles/buttons.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <Board />
     </div>
-  );
+  )
 }
 
 export default App;
